baf0f83bb1991b119a58dca2403f912c06191a63
matheus-provin <127682368+matheus-provin@users.noreply.github.com>
2023-07-18 09:08:48 -0300

    2

---
+++ mobileFood/src/components/ItemModal.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, Modal } from 'react-native';
+
+
+type FoodMenuModalProps = {
+  visible: boolean;
+  onClose: () => void;
+  item: string;
+  properties: string[];
+};
+
+const FoodMenuModal: React.FC<FoodMenuModalProps> = ({ visible, onClose, item, properties }) => {
+  const [selectedProperties, setSelectedProperties] = useState<string[]>([]);
+
+  const handlePropertyToggle = (property: string) => {
+    if (selectedProperties.includes(property)) {
+      setSelectedProperties(selectedProperties.filter((prop) => prop !== property));
+    } else {
+      setSelectedProperties([...selectedProperties, property]);
+    }
+  };
+
+  return (
+    <Modal visible={visible} animationType="slide" transparent>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>{item}</Text>
+          {properties.map((property, index) => (
+            <View key={index} style={styles.propertyContainer}>
+                
+              <Text style={styles.propertyText}>{property}</Text>
+            </View>
+          ))}
+          <Button title="Close Modal" onPress={onClose} />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '80%',
+    height: '90%',
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  propertyContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  propertyText: {
+    fontSize: 16,
+    marginLeft: 10,
+  },
+});
+
+export default FoodMenuModal;
--- mobileFood/src/screens/HomeScreen.tsx
+++ mobileFood/src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import {
   ScrollView,
@@ -7,17 +7,29 @@ import {
 import Swiper from "react-native-swiper";
 import ScrollableTextBox from "../components/Categorias";
 import FoodItem from "../components/ItemCardapio";
+import FoodMenuModal from "../components/ItemModal";
 
 type HomeScreenProps = {
   navigation: any; // You can define a proper type for navigation
 };
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("");
+
+  const handleItemPress = (item: string) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
   const handleTextClick = (item: string) => {
     console.log(`Clicked: ${item}`);
   };
 
-  const swiperRef = useRef<Swiper>(null);
   const categorias = [
     "Todos",
     "Bebidas",
@@ -26,7 +38,6 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
     "Salgados",
     "Outros",
   ];
-
 
   return (
     <GestureHandlerRootView style={styles.container}>
@@ -40,51 +51,49 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
           </View>
 
           <View style={styles.greenBox} />
-          <Text style={styles.title}>Cardárpio Digital</Text>
+          <Text style={styles.title}>Cardápio Digital</Text>
           <ScrollableTextBox
             categorias={categorias}
             onTextClick={handleTextClick}
           />
           <View>
-          <FoodItem
-            title="Pizza"
-              photo={
-                "https://blog.ceraflame.com.br/wp-content/uploads/2021/06/Pizza-Napolitana-CERAFLAME-480x270.jpg"
-              }
-              description={"Pizza de queijo, calabresa e tomate"}
-              price={"R$15"}
-              onPress={() => handleTextClick}
-            />
-          <FoodItem
-            title="Hamburguer"
-              photo={
-                "https://classic.exame.com/wp-content/uploads/2020/05/Vinil-Burger.jpg?quality=70&strip=info&w=1024"
-              }
-              description={"Hamburguer com cebola"}
-              price={"R$155"}
-              onPress={() => handleTextClick}
+            <FoodItem
+              title="Pizza"
+              photo="https://blog.ceraflame.com.br/wp-content/uploads/2021/06/Pizza-Napolitana-CERAFLAME-480x270.jpg"
+              description="Pizza de queijo, calabresa e tomate"
+              price="R$15"
+              onPress={() => handleItemPress("Pizza")}
             />
             <FoodItem
-            title="Sushi"
-              photo={
-                "https://www.ufrgs.br/laranjanacolher/wp-content/uploads/2021/08/1.png"
-              }
-              description={"Sushi irado"}
-              price={"R$15"}
-              onPress={() => handleTextClick}
+              title="Hamburguer"
+              photo="https://classic.exame.com/wp-content/uploads/2020/05/Vinil-Burger.jpg?quality=70&strip=info&w=1024"
+              description="Hamburguer com cebola"
+              price="R$155"
+              onPress={() => handleItemPress("Hamburguer")}
             />
             <FoodItem
-            title="Xis"
-              photo={
-                "https://www.rbsdirect.com.br/imagesrc/35719497.jpg?w=1024&h=1024&a=c"
-              }
-              description={"Xis tudo de bom"}
-              price={"R$15"}
-              onPress={() => handleTextClick}
+              title="Sushi"
+              photo="https://www.ufrgs.br/laranjanacolher/wp-content/uploads/2021/08/1.png"
+              description="Sushi irado"
+              price="R$15"
+              onPress={() => handleItemPress("Sushi")}
+            />
+            <FoodItem
+              title="Xis"
+              photo="https://www.rbsdirect.com.br/imagesrc/35719497.jpg?w=1024&h=1024&a=c"
+              description="Xis tudo de bom"
+              price="R$15"
+              onPress={() => handleItemPress("Xis")}
             />
           </View>
         </View>
       </ScrollView>
+      <FoodMenuModal
+        visible={modalVisible}
+        onClose={handleModalClose}
+        item={selectedItem}
+        properties={["Tomato", "Lettuce"]}
+      />
     </GestureHandlerRootView>
   );
 };
@@ -133,7 +142,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     display: "flex",
     textAlign: "center",
-  }
+  },
 });
 
 export default HomeScreen;
